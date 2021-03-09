52a3e345be8c71b69cf71d33d5eb67a9093ba81f
pelikhan <jhalleux@microsoft.com>
2021-03-09 10:00:25 +0100

    adding typescript support

--- content/introduction.md
+++ content/introduction.md
@@ -1,7 +1,5 @@
 ---
 title: "Introduction"
-metaTitle: "This is the title tag of this page"
-metaDescription: "This is the meta description"
 ---
 
 Some introduction text. Lists out all the headings from h1 to h6. Easy to customise.
--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const queries = require("./src/utils/algolia");
 const config = require("./config");
 const plugins = [
+  'gatsby-plugin-typescript',
   'gatsby-plugin-sitemap',
   'gatsby-plugin-sharp',
   {
